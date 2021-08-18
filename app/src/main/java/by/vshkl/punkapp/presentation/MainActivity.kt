3317ba2cf630d2f2b4a6e7ac09ffe575d69f58c2
Pavel Vashkel <p.vashkel@datarockets.com>
2021-08-18 21:59:44 +0200

    Render BeerList at the main screen

--- app/src/main/java/by/vshkl/punkapp/presentation/MainActivity.kt
+++ app/src/main/java/by/vshkl/punkapp/presentation/MainActivity.kt
@@ -1,37 +1,26 @@
 package by.vshkl.punkapp.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import by.vshkl.punkapp.presentation.ui.components.organism.BeerList
 import by.vshkl.punkapp.presentation.ui.theme.PunkAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PunkAppTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BeerList(beers = emptyList()) {
+                        Toast.makeText(this, it.id.toString(), Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PunkAppTheme {
-        Greeting("Android")
     }
 }
