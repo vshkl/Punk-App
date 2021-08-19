6fd0736d3c5b037551ac6f9d72ded3a5564310c5
Pavel Vashkel <p.vashkel@datarockets.com>
2021-08-19 19:58:36 +0200

    Add HttpLoggingInterceptor

--- app/src/main/java/by/vshkl/punkapp/Di.kt
+++ app/src/main/java/by/vshkl/punkapp/Di.kt
@@ -7,6 +7,8 @@ import by.vshkl.punkapp.data.repository.BeerRepository
 import by.vshkl.punkapp.framework.db.PunkDatabase
 import by.vshkl.punkapp.framework.network.PunkApi
 import by.vshkl.punkapp.presentation.feature.beers.BeersViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -14,8 +16,21 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 val appModule = module {
+    single {
+        OkHttpClient().newBuilder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if (BuildConfig.DEBUG)
+                        HttpLoggingInterceptor.Level.BODY else
+                        HttpLoggingInterceptor.Level.NONE
+                }
+            )
+            .build()
+    }
+
     single<Retrofit> {
         Retrofit.Builder()
+            .client(get())
             .baseUrl("https://api.punkapi.com/v2/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
